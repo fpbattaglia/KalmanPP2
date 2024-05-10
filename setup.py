e2de8e082197a55bc58e33477e537305f536100c
Francesco Battaglia <fpbattaglia@gmail.com>
2024-05-10 14:44:30 +0200

    Add setup.py for Psychophysics2 package
    A new setup.py file has been introduced for the Psychophysics2 package. This will enable easy installation and distribution of the package. The current version set is 0.1, authored by 'fpbattaglia'.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='Psychophysics2',
+	version=0.1,
+	url='',
+	license='',
+	author='fpbattaglia',
+	author_email='fpbattaglia at gmail.com',
+	description=''
+)
